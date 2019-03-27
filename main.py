245f576d663224bd7c358e85a8aec0b929bca854
Aleksey Rautkin <aleksey.rautkin@onetwotrip.com>
2019-03-27 19:59:09 +0300

    Fix bot sending

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import asyncio
 import json
+from asyncio import sleep
 from configparser import ConfigParser
 from typing import List, Dict
 
 from socks import SOCKS5
 import aioredis as aioredis
 from aiohttp import ClientSession, BasicAuth
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, executor
 
 
 async def save_task(task: Dict, config: ConfigParser):
@@ -17,16 +18,15 @@ async def save_task(task: Dict, config: ConfigParser):
         return await redis_connection.set(key=task['Id'], value=json.dumps(task))
 
 
-async def send_message(task: Dict, config: ConfigParser):
-    bot = Bot(config['BOT']['TOKEN'])
+async def send_message(task: Dict, config: ConfigParser, bot: Bot):
 
     await bot.send_message(config['CHANNEL']['ID'], text='test')
     # await bot.send_message(config['CHANNEL']['ID'], json.dumps(task))
 
 
-async def handle_tasks(tasks: List, config: ConfigParser):
+async def handle_tasks(tasks: List, config: ConfigParser, bot: Bot):
     for task in tasks:
-        await send_message(task, config)
+        await send_message(task, config, bot)
 
         # await save_task(task, config)
 
@@ -54,24 +54,24 @@ async def get_tasks(query: str):
 
 async def main(bot: Bot, config: ConfigParser):
 
-    await bot.send_message(config['CHANNEL']['ID'], text='test')
-
-    return
+    # await bot.send_message(config['CHANNEL']['ID'], text='test')
 
     search_queries = get_search_queries(config)
-    for query in search_queries:
-        tasks = await get_tasks(query)
-        await handle_tasks(tasks, config)
+    while True:
+        for query in search_queries:
+            tasks = await get_tasks(query)
+            await handle_tasks(tasks, config, bot)
+
+        await sleep(3)
 
 
 if __name__ == "__main__":
     config_parser = ConfigParser(comment_prefixes='#')
     config_parser.read('config.ini')
 
-    # PROXY_URL = 'socks5://51.144.86.230:18001'
-    # PROXY_AUTH = BasicAuth(login='usrTELE', password='avt231407')
-    # bot = Bot(config_parser['BOT']['TOKEN'], proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
-    bot = Bot(config_parser['BOT']['TOKEN'])
+    PROXY_URL = 'socks5://51.144.86.230:18001'
+    PROXY_AUTH = BasicAuth(login='usrTELE', password='avt231407')
+    bot = Bot(config_parser['BOT']['TOKEN'], proxy=PROXY_URL, proxy_auth=PROXY_AUTH)
 
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(main(bot, config_parser))
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(bot, config_parser))
